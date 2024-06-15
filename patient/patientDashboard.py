4c2e4ef32fc0539925483d1b10ddf9a97ba5ffa0
CodeWithSomesh <codewithsomesh@gmail.com>
2024-06-16 05:50:40 +0800

    Halfway done with debugging update appointment feature

--- patient/patientDashboard.py
+++ patient/patientDashboard.py
@@ -82,11 +82,69 @@ def patientDashboardWindow(email):
         searchInputTextBox.insert('0.0', "Search by Appointment Details")
         searchInputTextBox.configure(text_color='gray')
 
+    def updateAppointmentTopLevel():
 
-    def updateAppointment():
-        # Connecting to Appointment DB
-        appointmentConn = sqlite3.connect('appointments.db')
-        appointmentCursor = appointmentConn.cursor()
+        def updateAppointment():  
+
+            if (clinicName != '' and doctorType != '' and painDetails != '' and doctorName != '' and date != '' and 
+                time != '' and duration != ''):
+
+                if clinicName == 'Clinic Name':
+                    toplevel.attributes("-topmost",False)
+                    messagebox.showerror('Error',"Please select a Clinic.")
+                    if messagebox:
+                        toplevel.attributes("-topmost",True)
+                    return
+                
+                if doctorType == 'Select Type':
+                    toplevel.attributes("-topmost",False)
+                    messagebox.showerror('Error',"Please select the Type of Doctor.")
+                    if messagebox:
+                        toplevel.attributes("-topmost",True)
+                    return
+                
+                if doctorName == 'Select Doctor':
+                    toplevel.attributes("-topmost",False)
+                    messagebox.showerror('Error',"Please select a Doctor.")
+                    if messagebox:
+                        toplevel.attributes("-topmost",True)
+                    return
+                
+                if time == 'Select Time':
+                    toplevel.attributes("-topmost",False)
+                    messagebox.showerror('Error',"Please select the Consultaion Time.")
+                    if messagebox:
+                        toplevel.attributes("-topmost",True)
+                    return
+                
+                if duration == 'Select Duration':
+                    toplevel.attributes("-topmost",False)
+                    messagebox.showerror('Error',"Please select the Consultaion Duration.")
+                    if messagebox:
+                        toplevel.attributes("-topmost",True)
+                    return
+                
+
+                updateQuery = '''
+                    UPDATE appointments
+                    SET DoctorName = ?, DoctorType = ?, ClinicName = ?, AppointmentDate = ?, AppointmentTime = ?, AppointmentDuration = ?, PainDetails = ?
+                    WHERE AppointmentID = ? '''
+
+                appointmentCursor.execute(
+                    updateQuery, (doctorName, doctorType, clinicName, date, time, duration, painDetails, appointmentID))
+                appointmentConn.commit()
+
+                toplevel.attributes("-topmost",False)
+                messagebox.showinfo('Success', 'Appointment updated successfully.')
+                toplevel.destroy()
+                insertTreeview()
+                
+            
+            else:
+                toplevel.attributes("-topmost",False)
+                messagebox.showerror('Error',"Please fill up all the fields.")
+                if messagebox:
+                    toplevel.attributes("-topmost",True)
 
         selectedItem = table.focus()
         if not selectedItem:
@@ -98,9 +156,9 @@ def patientDashboardWindow(email):
         appointmentID = appointmentData[6]
         print(appointmentID)
 
-        
-        (clinicNameDropdown, doctorTypeDropdown, painDetailsTextBox, doctorDropdown, 
-                calendar, consultationTimeDropdown, consultationDurationDropdown, toplevel) = topLevel()
+        # Connecting to Appointment DB
+        appointmentConn = sqlite3.connect('appointments.db')
+        appointmentCursor = appointmentConn.cursor()
 
         appointmentCursor.execute('SELECT * FROM appointments WHERE AppointmentID=?', [appointmentID])
         result = appointmentCursor.fetchone()
@@ -114,77 +172,168 @@ def patientDashboardWindow(email):
         time = consultationTimeDropdown.set(result[10])
         duration = consultationDurationDropdown.set(result[11])
 
-        if (clinicName != '' and doctorType != '' and painDetails != '' and doctorName != '' and date != '' and 
-            time != '' and duration != ''):
-
-            if clinicName == 'Clinic Name':
-                toplevel.attributes("-topmost",False)
-                messagebox.showerror('Error',"Please select a Clinic.")
-                if messagebox:
-                    toplevel.attributes("-topmost",True)
-                return
-            
-            if doctorType == 'Select Type':
-                toplevel.attributes("-topmost",False)
-                messagebox.showerror('Error',"Please select the Type of Doctor.")
-                if messagebox:
-                    toplevel.attributes("-topmost",True)
-                return
-            
-            if doctorName == 'Select Doctor':
-                toplevel.attributes("-topmost",False)
-                messagebox.showerror('Error',"Please select a Doctor.")
-                if messagebox:
-                    toplevel.attributes("-topmost",True)
-                return
-            
-            if time == 'Select Time':
-                toplevel.attributes("-topmost",False)
-                messagebox.showerror('Error',"Please select the Consultaion Time.")
-                if messagebox:
-                    toplevel.attributes("-topmost",True)
-                return
-            
-            if duration == 'Select Duration':
-                toplevel.attributes("-topmost",False)
-                messagebox.showerror('Error',"Please select the Consultaion Duration.")
-                if messagebox:
-                    toplevel.attributes("-topmost",True)
-                return
-
-            print('Testing successful')
-
-            # appointmentCursor.execute(
-            #     'INSERT INTO appointments (PatientName, PatientID, DoctorName, DoctorID, DoctorType, DoctorAvailability, ClinicName, ClinicID, AppointmentDate, AppointmentTime, AppointmentDuration, AppointmentCreatedTime, PainDetails, IsConfirmed) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)', 
-            #     [patientName, patientID, doctorName, doctorID, doctorAvailability, doctorType, clinicName, clinicAdminID, date, time, duration, appointmentCreatedAt, painDetails, 0])
-            # appointmentConn.commit()
-
-            # if result:
-            #     newAppointments = currentAppointments + 1
-
-            #     # Update the number of appointments
-            #     patientCursor.execute('UPDATE patients SET NumberOfAppointments = ? WHERE Email= ?', (newAppointments, email))
-            #     patientConn.commit()
-
-            # patientConn.close()
-            
-
-            # toplevel.attributes("-topmost",False)
-            # messagebox.showinfo('Success', 'Appointment successfully added.')
-            # toplevel.destroy()
-            # insertTreeview()
-            
         
-        else:
-            toplevel.attributes("-topmost",False)
-            messagebox.showerror('Error',"Please fill up all the fields.")
-            if messagebox:
-                toplevel.attributes("-topmost",True)
+           
 
+        toplevel = ctk.CTkToplevel(window)
+        toplevel.title("Update Appointment")
+        toplevel.geometry("800x600+460+100")
+        toplevel.resizable(False, False)
+        toplevel.attributes("-topmost",True)
+        toplevel.configure(fg_color = "#fff")
+
+        # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< PARENT SCROLLABLE FRAME >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        parentFrame = ctk.CTkScrollableFrame(toplevel, width=800, height=600, fg_color="#fff", scrollbar_fg_color="#000", scrollbar_button_color="#000",)
+        parentFrame.place(x=0, y=0)
+
+        # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< TOP FRAME >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        topFrame = ctk.CTkFrame(parentFrame, width=650, height=500, fg_color="#FFFDFD" )
+        topFrame.pack(side='top', fill='x', expand=False, padx=(65, 80), pady=(20,0))
         
+        # Select Clinic Dropdown Menu
+        clinicNameLabel = ctk.CTkLabel(topFrame, text="Select Clinic", font=("Inter", 16, "bold",), anchor=ctk.W, text_color="#000000",)
+        clinicNameLabel.pack(side='top', fill='x', expand=False)
+        clinicNameDropdown = ctk.CTkComboBox(
+            topFrame, fg_color="#ffffff", text_color="#000000", width=295, height=48, 
+            font=("Inter", 20), button_color='#1AFF75', button_hover_color='#36D8B7',
+            values=['Clinic Name', 'Panmedic', 'Health Sync', 'Clinic Sungai Ara'], border_color="#b5b3b3", border_width=1,
+            dropdown_font=("Inter", 20), dropdown_fg_color='#fff', 
+            dropdown_text_color='#000', dropdown_hover_color='#1AFF75', hover=True,
+        )
+        clinicNameDropdown.pack(side='top', fill='x', expand=False,)
+
+        gMapsWidget = TkinterMapView(topFrame, width=650, height=400)
+        gMapsWidget.pack(side='top', fill='x', expand=True, pady=(10,0), padx=(0,5))
+        gMapsWidget.set_tile_server("https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)  # google normal
+        gMapsWidget.set_address("Bayan Lepas, Penang, Malaysia", marker=True)
+        gMapsWidget.set_zoom(12)
+
+        # gMapsWidget.set_tile_server("https://a.tile.openstreetmap.org/{z}/{x}/{y}.png")  # OpenStreetMap (default)
+        # gMapsWidget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=e...{x}&y={y}&z={z}&s=Ga", max_zoom=22)  # google satellite
+        # gMapsWidget.set_tile_server("http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.png")  # painting style
+        # marker.set_text("Select A Clinic")
+        
+
+        doctorTypeLabel = ctk.CTkLabel(topFrame, text="Select Type Of Doctor", font=("Inter", 16, "bold",), anchor=ctk.W, text_color="#000000",)
+        doctorTypeLabel.pack(side='top', fill='x', expand=False, pady=(30,0))
+        doctorTypes = [
+            "Select Type","Allergist", "Cardiologist", "Dermatologist", "Endocrinologist", 
+            "Gastroenterologist", "Geriatrician", "Internist", "Nephrologist", "Neurologist", 
+            "Obstetrician/Gynecologist", "Oncologist", "Ophthalmologist", "Orthopedic Surgeon", 
+            "Pediatrician", "Podiatrist", "Psychiatrist", "Pulmonologist", "Rheumatologist", 
+            "General Practitioner", "Family Medicine Doctor", "Home Health Care Doctor", 
+            "Emergency Medicine Specialist"]
+        doctorTypeDropdown = ctk.CTkComboBox(
+            topFrame, fg_color="#ffffff", text_color="#000000", width=295, height=48, 
+            font=("Inter", 20), button_color='#1AFF75', button_hover_color='#36D8B7',
+            values=doctorTypes, border_color="#b5b3b3", border_width=1,
+            dropdown_font=("Inter", 20), dropdown_fg_color='#fff', 
+            dropdown_text_color='#000', dropdown_hover_color='#1AFF75', hover=True,
+        )
+        doctorTypeDropdown.pack(side='top', fill='x', expand=False, pady=(0,0), padx=(0,5))
+
+        painDetailsLabel = ctk.CTkLabel(topFrame, text="Explain Pain Details", font=("Inter", 16, "bold",), anchor=ctk.W, text_color="#000000",)
+        painDetailsLabel.pack(side='top', fill='x', expand=False, pady=(30,0))
+        painDetailsTextBox = ctk.CTkTextbox(
+            topFrame, fg_color="#ffffff", text_color="#000000", width=648, height=88, 
+            border_color="#b5b3b3", font=("Inter", 20), border_spacing=10,
+            scrollbar_button_color="#1AFF75", border_width=1
+        )
+        painDetailsTextBox.pack(side='top', fill='none', expand=False, pady=(0, 10), anchor="w")
+
+
+        # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< LEFT FRAME INSDIE PARENT SCROLLABLE FRAME >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        leftFrame = ctk.CTkFrame(parentFrame, width=341, height=500, fg_color="#FFFDFD", )
+        leftFrame.pack(side='left', fill='both', expand=False, padx=65, pady=30)
+
+        doctorDropdownLabel = ctk.CTkLabel(leftFrame, text="Select Doctor", font=("Inter", 16, "bold",), anchor=ctk.W, text_color="#000000",)
+        doctorDropdownLabel.pack(side='top', fill='x', expand=False,)
+        doctorDropdown = ctk.CTkComboBox(
+            leftFrame, fg_color="#ffffff", text_color="#000000", width=295, height=48, 
+            font=("Inter", 20), button_color='#1AFF75', button_hover_color='#36D8B7',
+            values=['Select Doctor', 'Maisarah Majdi', 'Someshwar Rao', 'Karen Khor Siew Li'], border_color="#b5b3b3", border_width=1,
+            dropdown_font=("Inter", 20), dropdown_fg_color='#fff', 
+            dropdown_text_color='#000', dropdown_hover_color='#1AFF75', hover=True,
+        )
+        doctorDropdown.pack(side='top', fill='x', expand=False,)
+
+        times = [
+            "Select Time",'9:00 AM', '10:00 AM', '11:00 AM', 
+            '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', 
+            '6:00 PM', '7:00 PM', '8:00 PM', '9:00 PM', '10:00 PM', '11:00 PM',
+            '12:00 AM', '1:00 AM', '2:00 AM', '3:00 AM', '4:00 AM', '5:00 AM', 
+            '6:00 AM', '7:00 AM', '8:00 AM'
+        ]
+        consultationTimeDropdownLabel = ctk.CTkLabel(leftFrame, text="Select Consultation Time", font=("Inter", 16, "bold",), anchor=ctk.W, text_color="#000000",)
+        consultationTimeDropdownLabel.pack(side='top', fill='x', expand=False, pady=(150,0))
+        consultationTimeDropdown = ctk.CTkComboBox(
+            leftFrame, fg_color="#ffffff", text_color="#000000", width=295, height=48, 
+            font=("Inter", 20), button_color='#1AFF75', button_hover_color='#36D8B7',
+            values=times, border_color="#b5b3b3", border_width=1,
+            dropdown_font=("Inter", 20), dropdown_fg_color='#fff', 
+            dropdown_text_color='#000', dropdown_hover_color='#1AFF75', hover=True,
+        )
+        consultationTimeDropdown.pack(side='top', fill='x', expand=False,)
+
+        # Cancel Button with Icon
+        cancelIconPath = relative_to_assets("reject-icon.png")
+        cancelIcon = ctk.CTkImage(light_image=Image.open(cancelIconPath), size=(33,33),)
+        cancelButton = ctk.CTkButton(
+            leftFrame, text=" Cancel ", width=280, height=60, 
+            font=("Inter", 22, "bold",), fg_color="#E00000", hover_color="#AE0000", image=cancelIcon,
+            command=toplevel.destroy # anchor=ctk.W 
+        )
+        cancelButton.pack(side='top', fill='x', expand=False,pady=(40,15))
+
+
+        # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< RIGHT FRAME INSIDE PARENT SCROLLABLE FRAME >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        rightFrame = ctk.CTkFrame(parentFrame, width=341, height=500, fg_color="#FFFDFD",)
+        rightFrame.pack(side='left', fill='both', expand=False, padx=(0,0), pady=30)
+        
+
+        # Select Consultation Date Widget
+        consultationDateLabel = ctk.CTkLabel(rightFrame, text="Select Consultation Date", font=("Inter", 16, "bold",), anchor=ctk.W, text_color="#000000",)
+        consultationDateLabel.pack(side='top', fill='x', expand=False,)
+        calendar = Calendar(
+            rightFrame, selectmode="day", year=thisYear, font=("Inter", 12),
+            month=thisMonth, day=todayInt, background="#1AFF75", 
+            selectbackground="#36D8B7", foreground="black"
+        )
+        calendar.pack(side='top', fill='x', expand=False,)
+
+        # Select Consultation Duration Dropdown Menu
+        durationArray = [
+            'Select Duration', '1 hour', '1 hour 30 Minutes',
+            '2 hours', '2 hours 30 Minutes', '3 hours', '3 hour 30 Minutes', '4 hours', 
+            '4 hours 30 Minutes', '5 hours'
+        ]
+        consultationDurationDropdownLabel = ctk.CTkLabel(rightFrame, text="Select Consultation Duration", font=("Inter", 16, "bold",), anchor=ctk.W, text_color="#000000",)
+        consultationDurationDropdownLabel.pack(side='top', fill='x', expand=False, pady=(30,0))
+        consultationDurationDropdown = ctk.CTkComboBox(
+            rightFrame, fg_color="#ffffff", text_color="#000000", width=295, height=48, 
+            font=("Inter", 20), button_color='#1AFF75', button_hover_color='#36D8B7',
+            values=durationArray, border_color="#b5b3b3", border_width=1,
+            dropdown_font=("Inter", 20), dropdown_fg_color='#fff', 
+            dropdown_text_color='#000', dropdown_hover_color='#1AFF75', hover=True,
+        )
+        consultationDurationDropdown.pack(side='top', fill='x', expand=False,)
+
+        # Book Button 2 with Icon
+        updateIconPath = relative_to_assets("update-icon.png")
+        updateIcon = ctk.CTkImage(light_image=Image.open(updateIconPath), size=(33,33),)
+        updateButton = ctk.CTkButton(
+            rightFrame, text=" Update Appointment Details", height=48, width=378,
+            font=("Inter", 22, "bold",), fg_color="#1BC5DC", hover_color="#1695A7", image=updateIcon,
+            command=updateAppointment # anchor=ctk.W 
+        )
+        updateButton.pack(side='top', fill='x', expand=False,pady=(40,15))
 
     
-    def topLevel():
+
+      
+
+    
+    def bookAppointmentTopLevel():
 
         def bookAppointment():
             clinicName = clinicNameDropdown.get()
@@ -454,8 +603,6 @@ def patientDashboardWindow(email):
         bookButton2.pack(side='top', fill='x', expand=False,pady=(40,15))
         # bookButton2.bind("<Button-1>", command=bookAppointment)
 
-        return (clinicNameDropdown, doctorTypeDropdown, painDetailsTextBox, doctorDropdown, 
-                calendar, consultationTimeDropdown, consultationDurationDropdown, toplevel)
 
     global count
     count = 0
@@ -784,7 +931,7 @@ def patientDashboardWindow(email):
     bookButton = ctk.CTkButton(
         whiteFrame, text=" Book Appointment", width=290, height=48, 
         font=("Inter", 22, "bold",), fg_color="#17D463", hover_color="#009B2B", image=appointmentIcon,
-        command=topLevel # anchor=ctk.W 
+        command=bookAppointmentTopLevel # anchor=ctk.W 
     )
     bookButton.place(x=25, y=225)
 
@@ -794,7 +941,7 @@ def patientDashboardWindow(email):
     updateButton = ctk.CTkButton(
         whiteFrame, text=" Update Appointment Details", height=48, width=378,
         font=("Inter", 22, "bold",), fg_color="#1BC5DC", hover_color="#1695A7", image=updateIcon,
-        command=updateAppointment # anchor=ctk.W 
+        command=updateAppointmentTopLevel # anchor=ctk.W 
     )
     updateButton.place(x=333, y=225)
 
